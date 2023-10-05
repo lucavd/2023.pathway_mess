95a8750ade8be878f72b2107d936912bbdda0f54
Sara Ahsani-Nasab <93247424+Sara-Ahsani@users.noreply.github.com>
2023-10-05 15:24:26 +0200

    Update 04_map_through_mesh_clean.R

--- R/04_map_through_mesh_clean.R
+++ R/04_map_through_mesh_clean.R
@@ -244,3 +244,79 @@ write.csv(final_results_dfKEGG, "final_results_dfKEGG.csv")
 save(final_results_dfKEGG, file = 'final_resutsKEGG.rda')
 
 
+# WP ----------------------------------------------------------------------
+
+# Function to query, get genes and pathways
+query_to_pathwaysWP <- function(x) {
+  somma <- NA
+  matched_pathways <- character(0)
+  
+  tryCatch({
+    # Query execution
+    search_topic <- paste0('("', x, '"[Mesh]) AND ("2018/01/01"[Date - Entrez] : "2018/12/31"[Date - Entrez])')
+    search_query <- EUtilsSummary(search_topic, retmax = 1000)
+    
+    # Gene retrieval
+    filtered_data <- data.frame('PMID' = (search_query@PMID))
+    common_als <- intersect(pubtator$PMID, as.numeric(filtered_data$PMID))
+    annot_als <- pubtator[pubtator$PMID %in% common_als,]
+    
+    # Pathway analysis
+    WP_res <- enrichWP(gene = annot_als$Gene, organism = "Homo sapiens", 
+                       pAdjustMethod = "BH", 
+                       pvalueCutoff = 0.01, qvalueCutoff = 0.01,)
+    
+    path <- dplyr::tibble('Pathway' = WP_res@result[["Description"]],
+                          'q_value' = WP_res@result[["qvalue"]]) %>% 
+      dplyr::filter(q_value < 0.01)
+    
+    path$coviddi <- stringr::str_detect(path$Pathway, 
+                                        stringr::regex("COVID|coronavirus|SARS-CoV-2|COVID-19", 
+                                                       ignore_case = TRUE))
+    
+    somma <- sum(path$coviddi, na.rm = TRUE)
+    matched_pathways <- path$Pathway[path$coviddi]
+    
+  }, error = function(e) {
+    warning(paste("An error occurred for the term", x, ": ", e))
+  })
+  
+  return(list("sum" = somma, "matched_pathways" = matched_pathways))
+}
+
+# Function to execute pipeline for a given seed
+execute_pipelineWP <- function(seed) {
+  set.seed(seed)
+  terms_2 <- sample(terms, size = 2, replace = FALSE)
+  
+  results <- map(terms_2, query_to_pathwaysWP, .progress = "progress")
+  
+  somma_list <- map(results, 'sum')
+  matched_pathways_list <- map(results, 'matched_pathways')
+  
+  pathways_df <- dplyr::tibble('term' = terms_2, 
+                               'sum' = somma_list, 
+                               'matched_pathways' = matched_pathways_list,
+                               'seed' = seed)
+  
+  return(pathways_df)
+}
+
+# Execute pipeline for each seed and bind rows
+final_results_dfWP <- map_dfr(seeds, execute_pipelineWP, .progress = "progress")
+
+# If 'matched_pathways' is a list column in final_results_df
+final_results_dfWP$matched_pathways <- map_chr(final_results_dfWP$matched_pathways, function(x) {
+  paste(x, collapse = ",")
+})
+
+final_results_dfWP$sum <- map_chr(final_results_dfWP$sum, ~ {
+  if (is.na(.x)) return("NA")
+  return(as.character(.x))
+})
+
+write.csv(final_results_dfWP, "final_results_dfWP.csv")
+
+save(final_results_dfWP, file = 'final_resutsWP.rda')
+
+
